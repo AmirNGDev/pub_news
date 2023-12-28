e06aac47d6ce80a0a3d92bf239dc5bf8d69a47c4
Amir <ga7089036@gmail.com>
2023-12-28 18:13:42 +0330

    Refactored the code to use async/await instead of then for handling promises

--- lib/services/rss_to_json.dart
+++ lib/services/rss_to_json.dart
@@ -7,15 +7,13 @@ Future<dynamic> rssToJson() async {
   var client = http.Client();
   final MyTransformer = Xml2Json();
   try {
-    return await client.get(Uri.parse('https://pub.dev/feed.atom')).then((response) {
-      return response.body;
-    }).then((bodyString) {
-      MyTransformer.parse(bodyString);
-      var json = MyTransformer.toGData();
-      var jsonData = jsonDecode(json);
-      return jsonData;
-    });
-  }  catch (_) {
+    var response = await client.get(Uri.parse('https://pub.dev/feed.atom'));
+    var bodyString = response.body;
+    MyTransformer.parse(bodyString);
+    var json = MyTransformer.toGData();
+    var jsonData = jsonDecode(json);
+    return jsonData;
+  } catch (_) {
     print('Errorrrrrrrrrrrrrrr');
   }
 }
