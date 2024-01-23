1bc3d45895cc65a4012f633ff9d86e2140365e07
Amir <ga7089036@gmail.com>
2024-01-23 15:27:44 +0330

    Checking the platform and setting the minimum screen size

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:pub_news/routes/home.dart';
+import 'dart:io' show Platform;
+import 'package:window_size/window_size.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  if (Platform.isWindows) {
+        setWindowMinSize(const Size(1050, 500));
+    }
+
   runApp(const MyApp());
 }
 
@@ -10,13 +17,19 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    bool isAndroid = true;
+    if (Platform.isAndroid) {
+      isAndroid = true;
+    } else if (Platform.isWindows) {
+      isAndroid = false;
+    }
     return MaterialApp(
       title: 'Pub News',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
       ),
-      home: const HomeScreen(),
+      home: isAndroid ? const HomeScreen() : const WindowsScreen(),
     );
   }
 }
